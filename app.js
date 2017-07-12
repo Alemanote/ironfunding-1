7b17399f19b9df20fe87195aeebb433c455437c0
teddyviking <rtejado90@gmail.com>
2017-07-12 19:22:26 +0200

    create sign up form

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressLayouts = require('express-ejs-layouts');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const LocalStrategy      = require('passport-local').Strategy;
+const User               = require('./models/user');
+const bcrypt             = require('bcrypt');
 
 const index = require('./routes/index');
+const authRoutes = require('./routes/authentication');
 
 const app = express();
 mongoose.connect('mongodb://localhost:27017/ironfunds-development');
@@ -23,10 +30,50 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+	secret: 'ironfundingdev',
+	resave: false,
+	saveUninitialized: true,
+  store: new MongoStore( { mongooseConnection: mongoose.connection })
+}));
+
+passport.serializeUser((user, callback) => {
+  return callback(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use('local-signup', new LocalStrategy({passReqToCallback: true},
+	(req, username, password, next) => {
+			User.findOne({username}, (err, user) => {
+				if (err) return next(err)
+				if (user) return next(null, false, {message: 'User already exist'})
+				const {username, email, description, password} = req.body;
+				const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+				const newUser = new User({
+					username,
+					email,
+					description,
+					password: hashPass,
+				});
+				return newUser.save().then(user => next(null, user)).catch(error => next(error))
+			})
+	}
+))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components/')))
 
 app.use('/', index);
+app.use('/', authRoutes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
