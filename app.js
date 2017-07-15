d8ac29e677e623f18ef095985535cd51667ed763
teddyviking <rtejado90@gmail.com>
2017-07-15 08:18:24 +0200

    Refactor passport config

--- app.js
+++ app.js
@@ -6,13 +6,9 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressLayouts = require('express-ejs-layouts');
-const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
-const LocalStrategy      = require('passport-local').Strategy;
-const User               = require('./models/user');
-const bcrypt             = require('bcrypt');
-
+const passportConfig = require('./config/passport');
 const index = require('./routes/index');
 const authRoutes = require('./routes/authentication');
 
@@ -37,52 +33,7 @@ app.use(session({
 	store: new MongoStore( { mongooseConnection: mongoose.connection })
 }));
 
-passport.serializeUser((user, callback) => {
-	return callback(null, user.id);
-});
-
-passport.deserializeUser((id, cb) => {
-	User.findById(id, (err, user) => {
-		if (err) { return cb(err); }
-		cb(null, user);
-	});
-});
-
-passport.use('local-signup', new LocalStrategy({passReqToCallback: true},
-	(req, username, password, next) => {
-			User.findOne({username}, (err, user) => {
-				if (err) return next(err)
-				if (user) return next(null, false, {message: 'User already exist'})
-				const {username, email, description, password} = req.body;
-				const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8));
-				const newUser = new User({
-					username,
-					email,
-					description,
-					password: hashPass,
-				});
-				return newUser.save().then(user => next(null, user)).catch(error => next(error))
-			})
-	}
-))
-
-passport.use('local-login', new LocalStrategy({passReqToCallback: true},
-	(req, username, password, next) => {
-		User.findOne({ username }, (err, user) => {
-		 if (err) {
-			 return next(err);
-		 }
-		 if (!user) {
-			 return next(null, false, { message: "Incorrect username" });
-		 }
-		 if (!bcrypt.compareSync(password, user.password)) {
-			 return next(null, false, { message: "Incorrect password" });
-		 }
-		 return next(null, user);
-		});
-	}
-))
-
+const passport = passportConfig();
 app.use(passport.initialize());
 app.use(passport.session());
 
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
   email      : String,
@@ -8,6 +9,23 @@ const UserSchema = new Schema({
   description: String,
   imgUrl     : { type: String, default: "https://placeholdit.imgix.net/~text?txtsize=33&txt=250%C3%97250&w=250&h=250" }
 });
+
+UserSchema.statics.create = function(attributes, callback) {
+	const User = this;
+	const {username, email, description, password} = attributes;
+	const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+	const newUser = new User({
+		username,
+		email,
+		description,
+		password: hashPass,
+	});
+	return newUser.save().then(user => callback(null, user)).catch(error => callback(error))
+}
+
+UserSchema.methods.comparePassword = function(candidatePassword) {
+	return bcrypt.compareSync(candidatePassword, this.password)
+}
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
